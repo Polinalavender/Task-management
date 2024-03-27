3936854a51954a3916aee7f4a2920a16055f6d8c
Nathan P. Pais D'costa <71150111+nathan-pete@users.noreply.github.com>
2024-03-27 15:15:06 +0100

    Created nav and side bar

--- TaskManagement/TaskManagement/App.xaml.cs
+++ TaskManagement/TaskManagement/App.xaml.cs
@@ -6,7 +6,7 @@
         {
             InitializeComponent();
 
-            MainPage = new NavigationPage(new RegistrationPage());
+            MainPage = new NavigationPage(new TeamBoard());
         }
     }
 }
---
+++ TaskManagement/TaskManagement/TeamBoard.xaml.cs
@@ -0,0 +1,42 @@
+﻿using Microsoft.Maui.Controls;
+
+namespace TaskManagement
+{
+    public partial class TeamBoard : ContentPage
+    {
+        public TeamBoard()
+        {
+            InitializeComponent();
+        }
+
+        // Event handler for when a sidebar button is pressed
+        private void OnSideButtonPressed(object sender, EventArgs e)
+        {
+            var button = (Button)sender;
+            button.BackgroundColor = Colors.LavenderBlush;
+        }
+
+        // Event handler for when a sidebar button is released
+        private void OnSideButtonReleased(object sender, EventArgs e)
+        {
+            var button = (Button)sender;
+            button.BackgroundColor = Colors.White; // This should match the original button color
+            DisplayAlert("Side Bar", $"{button.Text} was clicked", "OK");
+        }
+
+        // Event handler for when a top bar button is clicked
+
+        private void OnNavButtonPressed(object sender, EventArgs e)
+        {
+            var button = (Button)sender;
+            button.BackgroundColor = Colors.LavenderBlush;
+        }
+
+        private void OnNavButtonReleased(object sender, EventArgs e)
+        {
+            var button = (Button)sender;
+            button.BackgroundColor = Colors.White; // This should match the original button color
+            DisplayAlert("Navigation", $"{button.Text} was clicked", "OK");
+        }
+    }
+}
