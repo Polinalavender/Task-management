84a31e0326b83c64f4f74926e3dc56ca147dd3fb
stefani-margaritova <91316578+stefani-margaritova@users.noreply.github.com>
2024-03-27 11:46:00 +0100

    Made Front end for task dashboard

---
+++ TaskManagement/TaskManagement/TaskManagerDashboard.xaml.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TaskManagement
+{
+    public partial class TaskManagerDashboard : ContentPage, INotifyPropertyChanged
+    {
+        public TaskManagerDashboard()
+        {
+            InitializeComponent();
+            BindingContext = this;
+        }
+    }
+}
