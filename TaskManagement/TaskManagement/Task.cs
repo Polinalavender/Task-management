a3ed3366f3ec7653475b010c9fe52b44fcd481cb
stefani-margaritova <91316578+stefani-margaritova@users.noreply.github.com>
2024-03-27 15:06:01 +0100

    Added some functionality to Task.cs

--- TaskManagement/TaskManagement/Task.cs
+++ TaskManagement/TaskManagement/Task.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using Microsoft.EntityFrameworkCore;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -8,15 +9,73 @@ namespace TaskManagement
 {
     public class Task
     {
-        public int  TaskId { get; set; }
-        public string Name { get; set; }
-        public string Deadline { get; set; }
+        public int TaskId { get; set; }
+        public string Name 
+        { 
+            get => Name; 
+            set 
+            {
+                if (string.IsNullOrEmpty(value))
+                {
+                    throw new ArgumentException("Name cannot be null or empty.");
+                }
+                Name = value;
+            } 
+        }
+        public string Deadline
+        {
+            get => Deadline;
+            set
+            {
+                if (string.IsNullOrWhiteSpace(value))
+                {
+                    throw new ArgumentException("Deadline cannot be null or empty.");
+                }
+                Deadline = value;
+            }
+        }
         public int AssignedUserID { get; set; }
-        public string TaskDescription { get; set; }
+        public string TaskDescription
+        {
+            get => TaskDescription;
+            set
+            {
+                if (string.IsNullOrWhiteSpace(value))
+                {
+                    throw new ArgumentException("Description cannot be null or empty.");
+                }
+                TaskDescription = value;
+            }
+        }
         public int TeamID { get; set; }
 
         // Navigation properties
         public Team Team { get; set; }
         public User AssignedUser { get; set; }
+
+        public Task(string name, string deadline)
+        {
+            Name = name;
+            Deadline = deadline;
+        }
+
+        // Methods
+        public void AssignTask(User user)
+        {
+            if (user == null)
+            {
+                throw new ArgumentNullException(nameof(user), "User cannot be null.");
+            }
+
+            AssignedUserID = user.UserId;
+            AssignedUser = user;
+
+            // save to db
+            using (var dbContext = new AppDbContext())
+            {
+                dbContext.Task.Update(this);
+                dbContext.SaveChanges();
+            }
+        }
     }
 }
