1a12e645ce464073a597652d0e3773f80dd070a3
Polina <91316935+Polinalavender@users.noreply.github.com>
2024-05-14 14:31:52 -0700

    Add new project

---
+++ TaskManager/TaskManager/MauiProgram.cs
@@ -0,0 +1,45 @@
+﻿using CommunityToolkit.Maui;
+using CommunityToolkit.Maui.Core;
+using DataAccessLayer.DbContext;
+using DataAccessLayer.Repositories;
+using Microsoft.Extensions.Logging;
+using Mopups.Hosting;
+using Mopups.Interfaces;
+using Mopups.PreBaked.PopupPages.Login;
+using Mopups.Services;
+using TaskManagement.Views;
+using TaskManager;
+
+namespace TaskManagement
+{
+    public static class MauiProgram
+    {
+        public static MauiApp CreateMauiApp()
+        {
+            var builder = MauiApp.CreateBuilder();
+            builder
+                .UseMauiApp<App>()
+                .UseMauiCommunityToolkit()
+                .ConfigureFonts(fonts =>
+                {
+                    fonts.AddFont("OpenSans-Regular.ttf", "OpenSansRegular");
+                    fonts.AddFont("OpenSans-Semibold.ttf", "OpenSansSemibold");
+                }).ConfigureMopups();
+            builder.Services.AddSingleton<IPopupNavigation>(MopupService.Instance);
+
+            builder.Services.AddTransient<TaskDbContext>();
+            builder.Services.AddSingleton<UserRepository>();
+            builder.Services.AddSingleton<TaskRepository>();
+            builder.Services.AddSingleton<TeamRepository>();
+
+            builder.Services.AddSingleton<AppShell>();
+            builder.Services.AddTransient<LoginViewModel>();
+
+#if DEBUG
+            builder.Logging.AddDebug();
+#endif
+
+            return builder.Build();
+        }
+    }
+}
